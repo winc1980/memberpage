808c916c7fa8d78de55db1949f06d1067227578c
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2023-09-08 20:24:41 +0900

    added sql file

---
+++ flask/app/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `profile` (
+	`first_name` VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+	`last_name` VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+	`team` VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+	`fac,univ` VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+	`self_intro` TEXT,
+	`img` TEXT
+);
