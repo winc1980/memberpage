629f3b03289f03f7733c315e59904430168e7614
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2023-09-08 17:53:09 +0900

    call index.html

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return 'Hello japan'
+
+@app.route("/htmlFile")
+def html_page():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
