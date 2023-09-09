eab9e662f65d7fb90a45f329dd26a68bd4a1d20c
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2023-09-09 20:36:51 +0900

    connect database to profile_form

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,14 +1,38 @@
 #http://localhost:5005/htmlFile
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import mysql.connector
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    my_variable = "こんにちは、Flask!"
-    return render_template('index.html', greeting=my_variable)
-    
+    return render_template('index.html',)
+
+# データベース接続
+conn = mysql.connector.connect(
+    host="memberpage-db-1",
+    user="root",
+    password="root",
+    database="flask"
+)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        name = request.form['name']
+        team = request.form['team']
+        post = request.form['post']
+        university = request.form['university']
+        introduction = request.form['introduction']
+
+        # MySQLにデータを挿入するSQLクエリを実行
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO members_table (name, team, role, univ_faculty, self_intro) VALUES (%s, %s, %s, %s, %s)", (name, team, post, university, introduction))
+        conn.commit()
+        cursor.close()
+
+        return render_template('index.html',)
 
 @app.route("/htmlFile")
 def html_page():
@@ -18,6 +42,10 @@ def html_page():
     local_dict["uni"] = "早稲田大学"
     local_dict["intro"] = "HPチームリーダーです。"
     return render_template('profile_card.html',global_dict=local_dict)
+
+@app.route("/postForm")
+def post_form():
+    return render_template('profile_form.html')
 
 if __name__ == '__main__':
     app.debug = True
