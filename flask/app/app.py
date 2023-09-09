52531ee25657d38161f679473a72259fa710390a
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2023-09-09 21:39:11 +0900

    edit app.py

--- flask/app/app.py
+++ flask/app/app.py
@@ -3,7 +3,17 @@
 from flask import Flask, render_template, request
 import mysql.connector
 
+conn = mysql.connector.connect(
+    host="memberpage-db-1",
+    user="root",
+    password="root",
+    database="flask"
+)
+
+
 app = Flask(__name__)
+
+cursor = conn.cursor()
 
 @app.route('/')
 def index():
@@ -36,12 +46,23 @@ def submit():
 
 @app.route("/htmlFile")
 def html_page():
-    local_dict = {}
-    local_dict["name"] = "鈴木風真"
-    local_dict["team"] = "HPチーム"
-    local_dict["uni"] = "早稲田大学"
-    local_dict["intro"] = "HPチームリーダーです。"
-    return render_template('profile_card.html',global_dict=local_dict)
+    #data_dict列
+    data_dict = []
+    data_dict_list = []
+    cursor.execute("SELECT * FROM profile")
+    for row in cursor.fetchall():
+        data_dict = {
+            "name": row[0],
+            "team": row[1],
+            "uni": row[2],
+            "intro": row[3],
+        }
+        data_dict_list.append(data_dict)
+        print(data_dict)
+
+    return render_template('profile_card.html',global_dict_list=data_dict_list)
+    
+    
 
 @app.route("/postForm")
 def post_form():
@@ -50,3 +71,6 @@ def post_form():
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
+
+cursor.close()
+conn.close()
